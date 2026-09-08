c62a2c9b681a025df529ad86c6eca855cb9f87e0
Pedro14012011 <reispedrosampaio@gmail.com>
2026-09-08 14:24:40 -0400

    add tiposDeDados_Array.js

---
+++ tiposDeDados_Array.js
@@ -0,0 +1,3 @@
+let frutas = ["maçã", "banana", "laranja", ]; // Array strings
+let numeros = [1, 2, 3, 4, 5]; // Array numeros
+let misto = [true, "texto", 42]; //Array com diferentes tipos de numero
