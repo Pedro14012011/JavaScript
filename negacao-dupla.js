89a78c76360ccda60b068ecaa4f0ebd830a5858a
Pedro14012011 <reispedrosampaio@gmail.com>
2026-09-15 16:02:02 -0400

    add negacao-dupla.js

---
+++ negacao-dupla.js
@@ -0,0 +1,10 @@
+// Valores Falsy
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+
+// Valores Truthy
+console.log(!!1);
+console.log(!!"Texto");
+console.log(!![]);
+console.log(!!{});
